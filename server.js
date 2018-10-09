952d71045b41035bc6aa64b15d549c9f7b24b87f
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-10-09 19:59:05 +0200

    Apply Mateusz's suggestions

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var fs = require('fs');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var stringifyFile;
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -15,10 +14,13 @@ app.get('/getNote', function(req, res) {
 });
 
 app.post('/updateNote/:note', function(req, res) {
-  fs.writeFile('./test.json', stringifyFile += req.params.note, function(err) {
+  fs.readFile('./test.json', 'utf-8', function(err, data) {
     if (err) throw err;
-    console.log('File updated');
-    res.send(stringifyFile);
+    fs.writeFile('./test.json', data += req.params.note, function(err) {
+      if (err) throw err;
+      console.log('File updated');
+      res.send(data);
+    });
   });
 });
 
